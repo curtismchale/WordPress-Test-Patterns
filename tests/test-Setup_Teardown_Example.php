56858fcf4ebcb43527f51947811ae2c47c6bffb4
Justin Sternberg <justin@dsgnwrks.pro>
2014-06-04 23:44:42 -0700

    Add documentation

--- tests/test-Setup_Teardown_Example.php
+++ tests/test-Setup_Teardown_Example.php
@@ -1,31 +1,43 @@
 <?php
 
 /**
- * Simple extended test class testing wp_insert_post
+ * Simple extended test class to demonstrate setup/teardown methods
  */
 class Setup_Teardown_Example extends WP_UnitTestCase {
+
+	/**
+	 * setUp is a built-in method that allows you to.. set up.. your unit testing data applicable to the tests you need
+	 */
 	function setUp() {
 		parent::setUp();
 
+		// Make a fake user
 		$this->author = new WP_User( $this->factory->user->create( array( 'role' => 'editor' ) ) );
 
-		$post = array(
+		// Make a temporary fake post
+		$this->post_id = wp_insert_post( array(
 			'post_author'  => $this->author->ID,
 			'post_status'  => 'publish',
 			'post_content' => rand_str(),
 			'post_title'   => rand_str(),
-		);
-
-		// insert a post
-		$this->post_id = wp_insert_post($post);
+		) );
 	}
 
+	/**
+	 * tearDown is a built-in method that allows you to reset any data you setup
+	 */
 	function tearDown() {
 		parent::tearDown();
+
+		// Delete fake temporary post
 		wp_delete_post( $this->post_id );
 	}
 
+	/**
+	 * Just a simple test that demonstrates setUp and tearDown is working
+	 */
 	function test_post_id_is_int() {
-		$this->assertIsInt( $this->post_id );
+		// Determine we actually have a post ID
+		$this->assertTrue( 0 !== $this->post_id && is_int( $this->post_id ) );
 	}
 }
